bdf49ebd81e0e28be46b69694eced03254ecceea
Gino05-star <luigiamore.official@gmail.com>
2025-10-21 23:31:57 +0100

    Step 2: add getComputerChoice()

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Hello World");
+function getComputerChoice() {
+    const r = Math.random();
+    if (r < 1/3) return "rock";
+    if (r < 2/3) return "paper";
+    return "scissors";
+}
+
+console.log(getComputerChoice());
